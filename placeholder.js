44ffe19ea0cbdb3e339e046f068bc12d2a920e14
marine-nag <71370369+marine-nag@users.noreply.github.com>
2021-05-30 22:16:10 +0300

    header

--- placeholder.js
+++ placeholder.js
@@ -115,11 +115,7 @@ define(function (require) {
                                 });
                               
                                 // === Creating PDF invoice
-                                var tableBody =  [
-                                          [ 'First', 'Second', 'Third', 'The last one' ],
-                                          [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
-                                          [ 'Val 1', 'Val 2', 'Val 3', 'Val 4' ]
-                                        ];
+                              
                                 
                                 var docDefinition = {
                                   info: {
@@ -129,17 +125,35 @@ define(function (require) {
                                     subject: "Invoice",
                                     keywords: "Invoice",
                                   },
-                                   content: [
+                                  header: {
                                     {
                                       toc: {
-                                        title: {text: 'INDEX', style: 'header'}
+                                        title: 
+                                          { 
+                                            text: 'Hello driver', 
+                                            style: 'header' 
+                                          }
                                       }
                                     },
                                     {
+                                        table: 
+                                        {
+                                            headerRows: 1,
+                                            body: [
+                                             [ 
+                                                 "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+                                                , "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunk"
+                                             ]
+                                            ]
+                                        }
+                                    }
+                                  },
+                                  content: [
+                                    /*{
                                       text: 'This is a header',
                                       style: 'header',
                                       tocItem: true
-                                    },
+                                    },*/
                                     {
                                       layout: 'lightHorizontalLines', // optional
                                       table: {
