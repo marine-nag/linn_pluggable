b20fddd942ff774ae43c753dde8643291cbbc5aa
marine-nag <71370369+marine-nag@users.noreply.github.com>
2021-05-30 22:45:06 +0300

    3242

--- placeholder.js
+++ placeholder.js
@@ -146,12 +146,12 @@ define(function (require) {
                                                  { 
                                                      text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
                                                      margin: [15, 3, 0, 17]
-                                                     , noWrap: false, height: 400
+                                                     , noWrap: false, alignment: 'justify', height: 400
                                                  }, 
                                                  {
                                                      text: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunk",
                                                      margin: [15, 3, 0, 17]
-                                                     , noWrap: false, height: 400
+                                                     , noWrap: false,  alignment: 'justify', height: 400
                                                  }
                                              ]
                                             ]
