a5d2060e3b51ff43f45919c59fa6bd95ca4ad6e6
marine-nag <71370369+marine-nag@users.noreply.github.com>
2021-05-30 22:25:35 +0300

    h

--- placeholder.js
+++ placeholder.js
@@ -139,7 +139,8 @@ define(function (require) {
                                         table: 
                                         {
                                             headerRows: 1,
-                                            heights: [220, 220],
+                                            heights: [820, 820],
+                                            minHeight: 420,
                                             body: [
                                              [ 
                                                  "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
