cac1b3f556a6cf2749794297f38d429e69655e25
marine-nag <71370369+marine-nag@users.noreply.github.com>
2021-05-31 09:30:55 +0300

    dr

--- placeholder.js
+++ placeholder.js
@@ -130,34 +130,12 @@ define(function (require) {
                                       toc: {
                                         title: 
                                           { 
-                                            text: 'Hello driver', 
-                                            style: 'header' 
+                                            text: 'Hello driver',  
+                                            style: 'sectionHeader'
                                           }
                                       }
                                     }
-                                    /*,
-                                    {
-                                        table: 
-                                        {
-                                            headerRows: 1,
-                                            heights: [820, 820],
-                                            minHeight: 420,
-                                            body: [
-                                             [ 
-                                                 { 
-                                                     text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-                                                     margin: [15, 3, 0, 90]
-                                                     ,height: 400
-                                                 }, 
-                                                 {
-                                                     text: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunk",
-                                                     margin: [15, 3, 0, 90]
-                                                     , noWrap: true,  alignment: 'justify', height: 400
-                                                 }
-                                             ]
-                                            ]
-                                        }
-                                    }*/
+                                    
                                   ],
                                   content: [  
                                     {  
@@ -165,7 +143,8 @@ define(function (require) {
                                             [  
                                                 {  
                                                     text: 'Customer NAme',  
-                                                    bold: true  
+                                                    bold: true,  
+                                                    style: 'sectionHeader'
                                                 },  
                                                 { text: '' },  
                                                 { text: '' },  
