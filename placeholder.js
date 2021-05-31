0876641907e7e3a8f751eda0dd4d936dafec347e
marine-nag <71370369+marine-nag@users.noreply.github.com>
2021-05-31 11:24:22 +0300

    sfgsdfg

--- placeholder.js
+++ placeholder.js
@@ -63,14 +63,17 @@ define(function (require) {
         // Generate Barcode
         $scope.textToBarCodeBase64 = function(textStr) {
             let canvas = document.createElement('canvas');
-            return bwipjs.toCanvas(canvas, {
+            
+            bwipjs.toCanvas(canvas, {
                     bcid:        'code128',       // Barcode type
                     text:        '0123456789',    // Text to encode
                     scale:       3,               // 3x scaling factor
                     height:      10,              // Bar height, in millimeters
                     includetext: true,            // Show human-readable text
                     textxalign:  'center',        // Always good to set this
-                });
+            });
+            
+             return canvas.toDataURL('image/png');
         };
          ///======
         // Try to get data by macros with type API
